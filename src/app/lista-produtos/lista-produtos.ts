908b5129f905fb243b93367b4f98a6ce0f40c255
Guilherme <guilherme.anthonysantos02@gmail.com>
2026-07-30 19:53:04 -0300

    atualizado lista-produtos.html

--- src/app/lista-produtos/lista-produtos.ts
+++ src/app/lista-produtos/lista-produtos.ts
@@ -1,9 +1,35 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Item } from './Item'
 
 @Component({
   selector: 'app-lista-produtos',
-  imports: [],
+  imports: [ FormsModule ],
   templateUrl: './lista-produtos.html',
   styleUrl: './lista-produtos.css',
 })
-export class ListaProdutos {}
+export class ListaProdutos {
+  descricao_produto? : string = 'Valor'
+  valor_unitario? : number = 0.0
+  listaItens : Item[] = []
+
+  addItem(){
+    //INSTANCIA DA CLASSE ITEM PASSANDO OS PARÂMETROS DO CONSTRUTOR
+    // let item = new Item(this.descricao_produto, this.valor_unitario)
+
+    //INSTÂNCIA DA CLASSE ITEM (CRIANDO O OBJETO item)
+    let item = new Item()
+    item.idProduto = this.listaItens.length + 1
+    item.descricaoProduto = this.descricao_produto
+    item.valorUnitario = this.valor_unitario
+
+    //ADICIONANDO OBJETO item AO ARRAY listaItens
+    this.listaItens.push(item)
+
+    //LIMPANDO AS PROPRIEDADES DA CLASSE
+    this.descricao_produto = ''
+    this.valor_unitario = 0.0
+
+
+  }
+}
